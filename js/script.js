65682858d150821e29e57585438815b78d3a28a2
jlizardi1 <122846079+jlizardi1@users.noreply.github.com>
2023-11-09 12:14:16 -0600

    home page changes

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ $(document).ready(function(){
      $(this).children(".dropdown-content").slideUp(200);
   });
  });
+//slider
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+slides[currentIndex].style.opacity = 0;
+    currentIndex = index;
+    slides[currentIndex].style.opacity = 1;
+}
+
+function nextSlide() {
+    showSlide((currentIndex + 1) % slides.length);
+}
+
+// Run nextSlide function every 3 seconds
+setInterval(nextSlide, 3000);
