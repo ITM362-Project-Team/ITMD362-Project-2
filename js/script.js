736485f03b0ee95895b49f3e71b3ef5673bf510b
lordtoasty1114 <kaustgen@hawk.iit.edu>
2023-11-09 14:24:18 -0600

    Combined the things

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ $(document).ready(function(){
      $(this).children(".dropdown-content").slideUp(200);
   });
  });
+
+ let themes = document.querySelectorAll('.lbl-toggle');
+
+Array.from(themes).forEach(label => {
+  label.addEventListener('keydown', e => {
+    32 === spacbar
+    13 === enter
+    if (e.which === 32 || e.which === 13) {
+      e.preventDefault();
+      label.click();
+    };
+  });
+});
